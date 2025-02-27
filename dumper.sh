e90aeb008b0c17f656f0adf25d0e25ef5768c61e
Jaswanth Mamidisetti <sunnyteja201@gmail.com>
2025-02-27 13:35:49 +0530

    Update dumper.sh

--- dumper.sh
+++ dumper.sh
@@ -1153,8 +1153,8 @@ commit_and_push(){
 
 if [[ -s "${PROJECT_DIR}"/.github_token ]]; then
 	GITHUB_TOKEN=$(< "${PROJECT_DIR}"/.github_token)	# Write Your Github Token In a Text File
-	[[ -z "$(git config --get user.email)" ]] && git config user.email "guptasushrut@gmail.com"
-	[[ -z "$(git config --get user.name)" ]] && git config user.name "Sushrut1101"
+	[[ -z "$(git config --get user.email)" ]] && git config user.email "sunnyteja201@gmail.com"
+	[[ -z "$(git config --get user.name)" ]] && git config user.name "JaswantTeja"
 	if [[ -s "${PROJECT_DIR}"/.github_orgname ]]; then
 		GIT_ORG=$(< "${PROJECT_DIR}"/.github_orgname)	# Set Your Github Organization Name
 	else
